028c2131536f3bdfb5cee4f47c777e80b03d05e1
Kesse Sefa <kessesefa1@gmail.com>
2020-04-07 19:07:04 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utility
+
+{
+    class Console
+    {
+        static public string userdetails(string question)
+        {
+            System.Console.Write(question);
+            return System.Console.ReadLine();
+        }
+
+    }
+}
